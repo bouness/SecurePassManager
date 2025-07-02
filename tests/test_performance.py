a82f0dc856c5297109df38a565fb7c6541983849
Youness <nmsuce@gmail.com>
2025-07-02 16:08:09 -0700

    Update test_performance.py

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -3,9 +3,7 @@ import timeit
 import unittest
 
 
-class TestPerformance(unittest.TestCase):
-    def test_encryption_speed(self):
-        setup = """
+PERFORMANCE_ASLS = """
 from security.crypto import CryptoManager
 import os
 crypto = CryptoManager()
@@ -13,7 +11,10 @@ salt = os.urandom(16)
 key = crypto.derive_key("test_password", salt)
 crypto.initialize_cipher(key)
 data = b"Test data" * 1000  # 9KB
-        """
+"""
+class TestPerformance(unittest.TestCase):
+    def test_encryption_speed(self):
+        setup = PERFORMANCE_ASLS
 
         time = timeit.timeit("crypto.encrypt(data)", setup=setup, number=100)
         print(f"\nEncryption time for 100x 9KB: {time:.2f}s")
